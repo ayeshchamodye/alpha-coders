f244a0bbe631ac225c3df25e843b444778769039
Ayesh chamodye <109528815+ayeshchamodye@users.noreply.github.com>
2023-04-22 08:31:44 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,31 +2,35 @@ import "./styles.css";
 import { GiHamburgerMenu } from "react-icons/gi";
 import Navbar from "./components/Navbar";
 import Title from "./components/Title";
+import scrollX from "react-native";
 import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function App() {
   const [showNav, setShowNav] = useState(false);
   return (
     <div className="App">
-      <header>
-        <div className="head">
-          <ul>
-            <li>
-              <GiHamburgerMenu
-                className="menu"
-                onClick={() => setShowNav(!showNav)}
-              />
-            </li>
-            <li>
-              <h3>ALPHA CODERS</h3>
-            </li>
-            <li>
-              <Title />
-            </li>
-          </ul>
-        </div>
-      </header>
-      {showNav && <Navbar />}
+      <scrollY className="scrollY">
+        <header>
+          <div className="head">
+            <ul>
+              <li>
+                <GiHamburgerMenu
+                  className="menu"
+                  onClick={() => setShowNav(!showNav)}
+                />
+              </li>
+              <li>
+                <h3>ALPHA CODERS</h3>
+              </li>
+              <li>
+                <Title />
+              </li>
+            </ul>
+          </div>
+        </header>
+        {showNav && <Navbar />}
+      </scrollY>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,90 @@
 import ".././styles.css";
+import { motion, AnimatePresence } from "framer-motion";
+
+const item = {
+  exit: {
+    opacity: 0,
+    height: 0,
+    transition: {
+      ease: "easeInOut",
+      duration: 0.3,
+      delay: 1.2
+    }
+  }
+};
 
 const Navbar = () => {
   return (
     <div className="sidenav active">
-      <ul>
-        <li>
-          <a className="hover-underline-animation">Home</a>
-        </li>
-        <li>
-          <a className="hover-underline-animation">About Us</a>
-        </li>
-        <li>
-          <a className="hover-underline-animation">Services</a>
-        </li>
-      </ul>
+      <AnimatePresence>
+        {open && (
+          <motion.div
+            className="menu_container"
+            variants={item}
+            initial={{ height: 0, opacity: 0 }}
+            animate={{ height: "100vh", opacity: 1 }}
+            transition={{ duration: 0.5 }}
+            exit="exit"
+          >
+            <ul>
+              <li>
+                <motion.a
+                  href=""
+                  initial={{ y: 80, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  transition={{ delay: 0.8 }}
+                  exit={{
+                    opacity: 0,
+                    y: 90,
+                    transition: {
+                      ease: "easeInOut",
+                      delay: 1
+                    }
+                  }}
+                >
+                  <a className="hover-underline-animation">Home</a>
+                </motion.a>
+              </li>
+              <li>
+                <motion.a
+                  href=""
+                  initial={{ y: 80, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  transition={{ delay: 0.7 }}
+                  exit={{
+                    opacity: 0,
+                    y: 90,
+                    transition: {
+                      ease: "easeInOut",
+                      delay: 0.8
+                    }
+                  }}
+                >
+                  <a className="hover-underline-animation">About</a>
+                </motion.a>
+              </li>
+              <li>
+                <motion.a
+                  href=""
+                  initial={{ y: 80, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  transition={{ delay: 0.6 }}
+                  exit={{
+                    opacity: 0,
+                    y: 90,
+                    transition: {
+                      ease: "easeInOut",
+                      delay: 0.6
+                    }
+                  }}
+                >
+                  <a className="hover-underline-animation">Services</a>
+                </motion.a>
+              </li>
+            </ul>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
